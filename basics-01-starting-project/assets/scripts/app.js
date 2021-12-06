128f2917a5c020f923d78c5ef31d4fb99cbafe53
vaibhav <vckotwad@gmail.com>
2021-12-06 12:37:16 +0530

    second commit

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const constant =100;
+let result=0;
+result=result+13
+result=result**2
+let calculations=`'13*13'  +  ${constant}`;
+outputResult(result+constant,calculations);
---
+++ basics-01-starting-project/assets/scripts/app1.js
@@ -0,0 +1,8 @@
+let a=23 ;
+let result=0;
+result = a +18;
+result=result*4;
+result=result/3;
+result=result-14;
+alert(a);
+alert(result);
